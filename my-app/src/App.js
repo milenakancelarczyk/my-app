e10280eb69f9a2dec567dcaebc2bcb5d81fcb783
milenakancelarczyk <m-kancelarczyk@wp.pl>
2021-07-02 14:31:56 +0200

    create new directories, run json server with meals, download and display meals

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,19 @@
 
-import './App.scss';
 
 function App() {
-  return (
+    const getMeals = () => {
+        fetch('http://localhost:3000/meals')
+            .then((res) => res.json())
+            .then((meals) => {
+                console.log(meals);
+            })
+    };
+    return (
+        <div className="App">
+            <button onClick={getMeals}>Pobierz meals</button>
+        </div>
+    )
 
-  );
 }
 
 export default App;
